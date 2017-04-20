3d45b3a8ee66edc092ff0026f2357b083f8dcd97
thinktwice13 <marioo.krajacic@gmail.com>
2017-04-20 17:08:32 +0200

    add table, fileupload, switcher component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,92 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
-class App extends Component {
+
+class Table extends Component {
   render() {
     return (
-      <div>
-        YAY
+      <div>Table here</div>
+    )
+  }
+}
+
+class FileUpload extends Component {
+  render() {
+    return (
+      <form>
+        <input
+          type="file" />
+        <button
+          type="submit">
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+class Switcher extends Component {
+  currChange(e) {
+    e.preventDefault();
+    this.props.onCurrChange(e.target.value);
+  }
+
+  render() {
+    return (
+      <select
+        onChange={this.currChange.bind(this)}>
+        {this.props.options.map(curr => {
+          return <option key={curr} value={curr}>{curr}</option>
+        })}
+      </select>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      base: this.props.currencies[0]
+    }
+
+    this.handleCurrChange = this.handleCurrChange.bind(this);
+    this.handleFileUpload = this.handleFileUpload.bind(this);
+  }
+
+  componentDidMount() {
+    //TODO: get exchange rates data
+  }
+
+  handleCurrChange(curr) {
+    this.setState({ base: curr });
+    //TODO: recalculate exchange rates data
+  }
+
+  //TODO: handle file upload
+  handleFileUpload() {
+    
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="selector">
+          <FileUpload
+            onChange={this.handleFileUpload}>
+          </FileUpload>
+          <Switcher
+            onCurrChange={this.handleCurrChange}
+            options={this.props.currencies} >
+          </Switcher>
+        </div>
+        <div>
+          <Table
+            rates="TODO">
+
+          </Table>
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const currencies = ["EUR","USD","JPY","CAD","CHF","GBP"];
+
 ReactDOM.render(
-  <App />,
+  <App
+    url="http://localhost:3001"
+    pollinterval={2000}
+    currencies={currencies} />,
   document.getElementById('root')
 );
